bae033ee4b302af04403468b59cf96c135f17011
Alejandro <alejamp@gmail.com>
2023-07-26 12:17:54 -0300

    add timeout sample to cinemark

--- prompts/cinemark/cinemark_ticketera.py
+++ prompts/cinemark/cinemark_ticketera.py
@@ -1,7 +1,9 @@
+import threading
+import time
 from lolapy import LolaSDK
 from lolapy import LolaContext
 from tokens import SUNDEVS_CINEMARK_TICKETERA_TOKEN
-
+import schedule
 
 lola = LolaSDK(
     lola_token=SUNDEVS_CINEMARK_TICKETERA_TOKEN, 
@@ -9,8 +11,12 @@ lola = LolaSDK(
     webhook_url='https://3ebb-181-168-225-209.ngrok-free.app')
 
 
+
+
+
+
 @lola.on_command('get_billboard')
-def handle_get_cryptocurrency_price(lead, ctx: LolaContext, request):
+def handle_get_cryptocurrency_price(session, ctx: LolaContext, request):
     city = request['data']['args']['city']
     movie = request['data']['args']['movie']
 
@@ -24,9 +30,8 @@ def handle_get_cryptocurrency_price(lead, ctx: LolaContext, request):
     return {'data': f'Please select a theater:\nAvailable theaters: Cinemark Plaza Nor, Cinemark Floresta'}
 
 
-
 @lola.on_command('get_billboard_for_a_theater')
-def handle_get_billboard_for_a_theater(lead, ctx: LolaContext, request):
+def handle_get_billboard_for_a_theater(session, ctx: LolaContext, request):
     name = request['data']['args']['name']
     
     # TODO: Get billboard from Cinemark API from that city
@@ -39,7 +44,7 @@ def handle_get_billboard_for_a_theater(lead, ctx: LolaContext, request):
     return {'data': f'Movies:\n-Barbie\n Showtime: 18:00, 20:00, 22:00'}
 
 @lola.on_command('do_checkout')
-def handle_do_checkout(lead, ctx: LolaContext, request):
+def handle_do_checkout(session, ctx: LolaContext, request):
     theater = request['data']['args']['theater']
     movie = request['data']['args']['movie']
     showtime = request['data']['args']['showtime']
@@ -55,12 +60,14 @@ def handle_do_checkout(lead, ctx: LolaContext, request):
 
 
 @lola.on_event('onTextMessage')
-def handle_text_message(lead, ctx: LolaContext, msg):
+def handle_text_message(session, ctx: LolaContext, msg):
     
     print(f'Got text message: {msg["text"]}')
     s = ctx.state.get()
     print(f'Current state: {s}')
     ctx.state.set({'counter': s.get('counter', 0) + 1})
+
+    ctx.timeout.set(session, ctx, 5, '5_seconds_without_message')
 
     # Testing send message to client
     # This message will be sent to the client
@@ -74,11 +81,15 @@ def handle_text_message(lead, ctx: LolaContext, msg):
     # return f'You said2: {msg["text"]}'
 
 
+@lola.on_timeout()
+def handle_timeout(session, ctx: LolaContext, label):
+    print(f'Timeout reached for label: {label}')
+    ctx.messanger.send_text_message(f'Timeout reached for label: {label}')
 
 # On each new conversation, this event will be triggered
 # This is useful to initialize the state of the conversation
 @lola.on_event('onNewConversation')
-def handle_new_conversation(lead, ctx: LolaContext, msg):
+def handle_new_conversation(session, ctx: LolaContext, msg):
     
     print(f'Got new conversation message: {msg["text"]}')
 
@@ -103,6 +114,7 @@ def handle_new_conversation(lead, ctx: LolaContext, msg):
     # This return will be sent to the client and will interrupt the flow to the AI
     # Uncomment this if you want to send a message to the client without AI intervention
     # return f'You said: {msg["text"]}'
+
 
 
 
